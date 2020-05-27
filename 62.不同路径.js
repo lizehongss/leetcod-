50e7cd39a4a5656b3384dfa7ea88a25be7b589f5
lizehongss <13600484164@163.com>
2020-05-27 17:17:04 +0800

    添加

---
+++ 62.不同路径.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+  var dp = new Array(m);
+  for(var i = 0;i<n;i++){
+      dp[i] = new Array(n);
+      dp[i][0] = 1;
+  }
+  for(var r = 0;r < m;r++){
+      dp[0][r] = 1;
+  }
+  for(var j = 1;j<n;j++){
+      for(var z = 1;z<m;z++){
+          dp[j][z] = dp[j-1][z]+dp[j][z-1]
+      }
+  }
+  return dp[n-1][m-1];
+};
+// @lc code=end
+
