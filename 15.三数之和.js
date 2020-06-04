d37ddcf4ad22889f2b8b9dbce29c4f63d9c919e6
lizehongss <13600484164@163.com>
2020-06-04 17:46:50 +0800

    添加

---
+++ 15.三数之和.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=15 lang=javascript
+ *
+ * [15] 三数之和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+ function quickSort(arr,left, right) {
+  if (left < right) {
+    var pivot = right
+    var partitonIndex = partition(arr, pivot, left, right)
+    quickSort(arr, left, partitonIndex - 1 < left ? left : partitonIndex - 1)
+    quickSort(arr, partitonIndex + 1 > right ? right : partitonIndex + 1, right)
+  }
+
+ }
+ function partition(arr, sign, start, end) {
+ let startIndex = start
+ for(let  i = start; i < end; i++) {
+   if(arr[i] < arr[sign]) {
+     swap (arr, i, startIndex)
+     startIndex ++
+   }
+ }
+ swap(arr, startIndex, sign)
+ return startIndex
+}
+ function swap (arr, i, j) {
+  var temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+let result = []
+ // 排序
+ quickSort(nums, 0, nums.length - 1)
+ if (nums.length <3) return []
+ for(let i = 0; i<nums.length; i++) {
+   let L = i
+   let R = nums.length -1
+   while(L<R) {
+     if (nums[L] + nums[L] +nums[R] === 0) {
+       result.push([nums[i], nums[L], nums[R]])
+       while(L<R&&nums[L] === nums[L+1]) {
+         L=L+1
+       }
+       while(L<R && nums[R] === nums[R-1]) {
+         R=R-1
+       }
+       L = L+1
+       R = R+1
+     } else if (nums[L] + nums[L] +nums[R] > 0) {
+       R = R-1
+     } else {
+       L = L+1
+     }
+   }
+ }
+
+};
+// @lc code=end
+
