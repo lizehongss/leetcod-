068448d3293a92fb7c0f134d19d5b7d6519ddc21
lizehongss <13600484164@163.com>
2020-06-09 17:21:10 +0800

    添加

--- 150.逆波兰表达式求值.js
+++ 150.逆波兰表达式求值.js
@@ -9,6 +9,32 @@
  * @param {string[]} tokens
  * @return {number}
  */
+// var evalRPN = function(tokens) {
+//   let fun = () => {
+//       let char = tokens.pop();
+//       let num;//由于减法和除法的存在顺序问题
+//       switch (char) {
+//       case "+":
+//           return fun() + fun();
+//       case "-":
+//           num = fun();
+//           return fun() - num;
+//       case "*":
+//           return fun() * fun();
+//       case "/":
+//           num = fun();
+//           return parseInt(fun() / num, 10);
+//       default:
+//           return parseInt(char, 10);
+//       }    
+//   }
+//   return fun();
+// };
+
+// 作者：lvshanke
+// 链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/solution/dan-ke-xi-lie-yong-shi-9982nei-cun-10000-by-lvsh-2/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 var evalRPN = function(tokens) {
   let numsStack = []
   let flag = ['+', '-', '*', '/']
