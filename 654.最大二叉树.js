5d702c9f914144837c1d90b726a598a3965a8841
lizehongss <13600484164@163.com>
2021-11-10 22:54:14 +0800

    添加题解

---
+++ 654.最大二叉树.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=654 lang=javascript
+ *
+ * [654] 最大二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var constructMaximumBinaryTree = function(nums) {
+    if (nums.length === 0) return null
+    let max = nums[0]
+    let index = 0
+    for(let i = 0; i< nums.length; i++ ) {
+        if(nums[i] > max) {
+            max = nums[i]
+            index = i
+        }
+    }
+    let root = new TreeNode(max)
+    root.left = constructMaximumBinaryTree(nums.slice(0, index))
+    root.right = constructMaximumBinaryTree(nums.slice(index+1, nums.length))
+    return root
+};
+// @lc code=end
+
